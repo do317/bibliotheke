05afd54744b82ad3bcc13cb564d9f38ead4691ef
do317 <246140219+do317@users.noreply.github.com>
2026-04-28 14:33:40 +0300

    Create numpy.py

---
+++ numpy.py
@@ -0,0 +1,9 @@
+import numpy
+
+# Uztaisa objektu, ar kuru var viegli darboties.
+print(numpy.array([1, 2, 3]))
+#[1 2 3]
+
+# Saskaita 2 numpy.array objektu elementus
+print(numpy.add(numpy.array([1, 2, 3]),numpy.array([1, 2, 3])))
+#[2 4 6]
