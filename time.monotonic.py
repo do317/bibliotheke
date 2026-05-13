7f187bcedd88a11edb4f6b425a1973757c888276
do317 <246140219+do317@users.noreply.github.com>
2026-05-13 13:05:27 +0300

    Update time.monotonic.py

--- time.monotonic.py
+++ time.monotonic.py
@@ -1,4 +1,4 @@
 import time
 
-# time.monotonic iedod skaitli kas paliek lielāks ar laiku.
+# time.monotonic iedod skaitli kas paliek lielāks ar laiku, pat ja kāds maina pulksteni.
 print(time.monotonic())
