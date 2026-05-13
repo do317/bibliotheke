1d1a95911354c0137a5730bb27c8b49271a1f29e
do317 <246140219+do317@users.noreply.github.com>
2026-05-13 13:01:19 +0300

    vairāk par numpy

--- numpy.py
+++ numpy.py
@@ -1,9 +1,11 @@
 import numpy
 
-# Uztaisa objektu, ar kuru var viegli darboties.
+# Uztaisa objektu, ar kuru var viegli darīt matemātiskas darbības.
 print(numpy.array([1, 2, 3]))
 #[1 2 3]
 
-# Saskaita 2 numpy.array objektu elementus
+# Saskaita divu numpy.array objektu elementus
 print(numpy.add(numpy.array([1, 2, 3]),numpy.array([1, 2, 3])))
 #[2 4 6]
+# var arī izmantot +
+print(numpy.array([1, 2, 3]) + numpy.array([1, 2, 3]))
